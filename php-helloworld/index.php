c16963697aa3c71d246b87af819f93a97868c2d9
Student User <student@workstation.lab.example.com>
2022-05-24 15:57:28 -0400

    Have a nice day.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "This is a test of a test, but who is watching the tester?";
+print "Logging off now, have a nice day.";
 ?>
